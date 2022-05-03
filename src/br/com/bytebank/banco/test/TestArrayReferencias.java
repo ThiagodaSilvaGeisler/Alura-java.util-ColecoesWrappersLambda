78daaf420bdbe6a55e0f71f3d65cd5e54002a8fe
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-03 04:02:31 -0300

    Usando type casta, transformando uma referência genérica em uma mais específica

--- src/br/com/bytebank/banco/test/TestArrayReferencias.java
+++ src/br/com/bytebank/banco/test/TestArrayReferencias.java
@@ -1,6 +1,8 @@
 package br.com.bytebank.banco.test;
 
+import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.ContaPoupanca;
 
 public class TestArrayReferencias {
 
@@ -8,15 +10,17 @@ public class TestArrayReferencias {
 	
 	public static void main(String[] args) {
 	
-	ContaCorrente[] contas = new ContaCorrente[5];
+	Conta[] contas = new Conta[5];
+	
 	ContaCorrente cc1 = new ContaCorrente(22,11);
 	contas[0] = cc1;
 	
-	ContaCorrente cc2 = new ContaCorrente(22,22);
+	ContaPoupanca cc2 = new ContaPoupanca(22,22);
 	contas[1] = cc2;
 	
 	System.out.println(contas[1].getNumero());
-	ContaCorrente ref = contas[1];
+	
+	ContaPoupanca ref =(ContaPoupanca) contas[1]; //type cast
 	System.out.println(cc2.getNumero());
 	System.out.println(ref.getNumero());
 

6af817f185a265ae59512a23d42b4ba22fb5c679
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-02 22:14:45 -0300

    Usando laço com o array

--- src/br/com/bytebank/banco/test/Teste.java
+++ src/br/com/bytebank/banco/test/Teste.java
@@ -12,18 +12,14 @@ public class Teste {
 
 		int[] idades = new int[5];
 
-		idades[0] = 29;
-		idades[1] = 39;
-		idades[2] = 49;
-		idades[3] = 59;
-		idades[4] = 69;
+		for(int i = 0; i< idades.length; i++) {
+			idades[i] = i * i;
+			System.out.println(idades[i]);
+		}
 		
-		int idade4 = idades[4];
-		System.out.println(idade4);
-		System.out.println(idades[4]);
-		
-		//Perguntar o tamanho para o array
-		System.out.println(idades.length);
+		for(int i= 0; i< idades.length; i++) {
+			System.out.println(idades[i]);
+		}
 		
 		
 	}

cef8986be4f7b6a42457be376487f3aae3fe55c5
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-03 11:40:59 -0300

    Usando a classe ArrayList

---
+++ src/br/com/bytebank/banco/test/util/Teste.java
@@ -0,0 +1,53 @@
+package br.com.bytebank.banco.test.util;
+
+import java.util.ArrayList;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		ArrayList lista = new ArrayList();
+
+		Conta cc = new ContaCorrente(22, 11);
+		lista.add(cc);
+
+		Conta cc2 = new ContaCorrente(22, 22);
+		lista.add(cc);
+
+		System.out.println("Tamanho: " + lista.size());
+
+		Conta ref = (Conta) lista.get(0);
+		
+		System.out.println(ref.getNumero());
+		
+		lista.remove(0);
+		
+		System.out.println("Tamanho: " + lista.size());
+		
+		Conta cc3 = new ContaCorrente(33, 311);
+		lista.add(cc3);
+
+		Conta cc4 = new ContaCorrente(33, 322);
+		lista.add(cc4);
+		
+		
+		for( int i = 0; i < lista.size(); i++) {
+			
+			Object oRef = lista.get(i);
+			System.out.println(oRef);
+		}
+		
+		System.out.println("------------------");
+		
+		// Há uma forma mais moderna de fazer a iteração sobre a lista (hoje em dia, é mais usado):
+		
+		for(Object oRef : lista) {
+			System.out.println(oRef);
+		}
+		
+	}
+
+}

07f9de2bf2fa476d7a3617e596ae0172df166a2f
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-03 04:06:48 -0300

    Criando um array genérico do tipo Object

--- src/br/com/bytebank/banco/test/TestArrayReferencias.java
+++ src/br/com/bytebank/banco/test/TestArrayReferencias.java
@@ -1,6 +1,6 @@
 package br.com.bytebank.banco.test;
 
-import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 
@@ -10,17 +10,20 @@ public class TestArrayReferencias {
 	
 	public static void main(String[] args) {
 	
-	Conta[] contas = new Conta[5];
+	Object[] referencias = new Object[5];
 	
 	ContaCorrente cc1 = new ContaCorrente(22,11);
-	contas[0] = cc1;
+	referencias[0] = cc1;
 	
 	ContaPoupanca cc2 = new ContaPoupanca(22,22);
-	contas[1] = cc2;
+	referencias[1] = cc2;
 	
-	System.out.println(contas[1].getNumero());
+	Cliente cliente = new Cliente();
+	referencias[2] = cliente;
 	
-	ContaPoupanca ref =(ContaPoupanca) contas[1]; //type cast
+	//System.out.println(referencias[1].getNumero());
+	
+	ContaPoupanca ref =(ContaPoupanca) referencias[1]; //type cast
 	System.out.println(cc2.getNumero());
 	System.out.println(ref.getNumero());
 
